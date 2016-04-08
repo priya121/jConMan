6416cc77e49fd405575d9f3efef15754333bbbc8
priya121 <priya121@github.com>
2016-04-08 16:35:36 +0100

    Adds read method to read a created contact name.

--- src/ConMan.java
+++ src/ConMan.java
@@ -1,5 +1,12 @@
 public class ConMan {
+    private String contact;
+
     public String create(String name) {
+        contact = name;
         return name;
+    }
+
+    public String read() {
+        return contact;
     }
 }
--- src/ConManTest.java
+++ src/ConManTest.java
@@ -3,11 +3,16 @@ import org.junit.Test;
 import static org.junit.Assert.assertEquals;
 
 public class ConManTest {
+    ConMan conMan = new ConMan();
 
     @Test
     public void createReturnsAContactName() {
-        ConMan conMan = new ConMan();
         assertEquals("Priya", conMan.create("Priya"));
     }
 
+    @Test
+    public void readReturnsCreatedContact() {
+        conMan.create("Priya");
+        assertEquals("Priya", conMan.read());
+    }
 }
